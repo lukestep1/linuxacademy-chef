151e6210dd5c2ab03e7bd3526cc55be9301d6858
root <root@bolabowale5.mylabserver.com>
2016-07-15 17:38:06 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["bolabowale6"] = { "site_title" => "bolabowale6s site coming soon", "port" => 80, "domain" => "bolabowale6.mylabserver.com" }
+default["apache"]["sites"]["bolabowale6b"] = { "site_title" => "bolabowale6bs site coming soon!", "port" => 80, "domain" => "bolabowale6b.mylabserver.com" }
